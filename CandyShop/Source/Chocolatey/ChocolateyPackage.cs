0df1160058f9d824f9d916435382c2aac6014f79
weberjonathan <jonathanwe97@gmail.com>
2020-09-17 19:03:48 +0200

    removed Outdated (unnecessary property)

--- CandyShop/Source/Chocolatey/ChocolateyPackage.cs
+++ CandyShop/Source/Chocolatey/ChocolateyPackage.cs
@@ -6,6 +6,5 @@
         public string CurrVer { get; set; }
         public string AvailVer { get; set; }
         public bool Pinned { get; set; }
-        public bool Outdated { get; set; }
     }
 }
