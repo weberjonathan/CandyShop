6a2e683d0adb3095f1ea5992ff59e67b9438b8e9
weberjonathan <jonathanwe97@gmail.com>
2025-06-08 18:02:47 +0200

    fixed text layout to prevent cutoff

--- CandyShop/View/SettingsWindow.Designer.cs
+++ CandyShop/View/SettingsWindow.Designer.cs
@@ -35,6 +35,7 @@
             btnOk = new System.Windows.Forms.Button();
             btnApply = new System.Windows.Forms.Button();
             panel1 = new System.Windows.Forms.Panel();
+            checkEnableGsudo = new System.Windows.Forms.CheckBox();
             lblSecurity2 = new System.Windows.Forms.Label();
             lblSecurity1 = new System.Windows.Forms.Label();
             label7 = new System.Windows.Forms.Label();
@@ -53,7 +54,6 @@
             btnWingetBinary = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
             txtWingetBinary = new System.Windows.Forms.TextBox();
-            checkEnableGsudo = new System.Windows.Forms.CheckBox();
             panel2.SuspendLayout();
             panel3.SuspendLayout();
             panel1.SuspendLayout();
@@ -65,17 +65,18 @@
             panel2.Controls.Add(lblChooseSource);
             panel2.Dock = System.Windows.Forms.DockStyle.Top;
             panel2.Location = new System.Drawing.Point(0, 0);
+            panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new System.Drawing.Size(648, 47);
+            panel2.Size = new System.Drawing.Size(741, 63);
             panel2.TabIndex = 2;
             panel2.Visible = false;
             // 
             // lblChooseSource
             // 
             lblChooseSource.AutoSize = true;
-            lblChooseSource.Location = new System.Drawing.Point(9, 16);
+            lblChooseSource.Location = new System.Drawing.Point(10, 21);
             lblChooseSource.Name = "lblChooseSource";
-            lblChooseSource.Size = new System.Drawing.Size(566, 15);
+            lblChooseSource.Size = new System.Drawing.Size(709, 20);
             lblChooseSource.TabIndex = 29;
             lblChooseSource.Text = "Welcome to Candy Shop. Please review the settings below and change them as needed before continuing.";
             // 
@@ -85,25 +86,28 @@
             panel3.Controls.Add(btnOk);
             panel3.Controls.Add(btnApply);
             panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            panel3.Location = new System.Drawing.Point(0, 335);
+            panel3.Location = new System.Drawing.Point(0, 446);
+            panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             panel3.Name = "panel3";
-            panel3.Size = new System.Drawing.Size(648, 53);
+            panel3.Size = new System.Drawing.Size(741, 71);
             panel3.TabIndex = 36;
             // 
             // btnOk
             // 
-            btnOk.Location = new System.Drawing.Point(555, 15);
+            btnOk.Location = new System.Drawing.Point(634, 20);
+            btnOk.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             btnOk.Name = "btnOk";
-            btnOk.Size = new System.Drawing.Size(75, 23);
+            btnOk.Size = new System.Drawing.Size(86, 31);
             btnOk.TabIndex = 10;
             btnOk.Text = "&OK";
             btnOk.UseVisualStyleBackColor = true;
             // 
             // btnApply
             // 
-            btnApply.Location = new System.Drawing.Point(474, 15);
+            btnApply.Location = new System.Drawing.Point(542, 20);
+            btnApply.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             btnApply.Name = "btnApply";
-            btnApply.Size = new System.Drawing.Size(75, 23);
+            btnApply.Size = new System.Drawing.Size(86, 31);
             btnApply.TabIndex = 9;
             btnApply.Text = "&Apply";
             btnApply.UseVisualStyleBackColor = true;
@@ -131,45 +135,58 @@
             panel1.Controls.Add(label1);
             panel1.Controls.Add(txtWingetBinary);
             panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            panel1.Location = new System.Drawing.Point(0, 47);
+            panel1.Location = new System.Drawing.Point(0, 63);
+            panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new System.Drawing.Size(648, 388);
+            panel1.Size = new System.Drawing.Size(741, 517);
             panel1.TabIndex = 3;
+            // 
+            // checkEnableGsudo
+            // 
+            checkEnableGsudo.AutoSize = true;
+            checkEnableGsudo.Location = new System.Drawing.Point(21, 276);
+            checkEnableGsudo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            checkEnableGsudo.Name = "checkEnableGsudo";
+            checkEnableGsudo.Size = new System.Drawing.Size(121, 24);
+            checkEnableGsudo.TabIndex = 37;
+            checkEnableGsudo.Text = "Enable gsudo";
+            checkEnableGsudo.UseVisualStyleBackColor = true;
             // 
             // lblSecurity2
             // 
             lblSecurity2.BackColor = System.Drawing.SystemColors.Control;
             lblSecurity2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
-            lblSecurity2.Location = new System.Drawing.Point(454, 178);
+            lblSecurity2.Location = new System.Drawing.Point(519, 252);
             lblSecurity2.Name = "lblSecurity2";
-            lblSecurity2.Size = new System.Drawing.Size(176, 128);
+            lblSecurity2.Size = new System.Drawing.Size(201, 160);
             lblSecurity2.TabIndex = 7;
             lblSecurity2.Text = "If you do not understand the security implications of this feature, it is strongly recommended to leave the option to cache administrator privileges unchecked.";
             // 
             // lblSecurity1
             // 
             lblSecurity1.BackColor = System.Drawing.SystemColors.Control;
-            lblSecurity1.Location = new System.Drawing.Point(454, 15);
+            lblSecurity1.Location = new System.Drawing.Point(519, 20);
             lblSecurity1.Name = "lblSecurity1";
-            lblSecurity1.Size = new System.Drawing.Size(176, 162);
+            lblSecurity1.Size = new System.Drawing.Size(201, 236);
             lblSecurity1.TabIndex = 6;
             lblSecurity1.Text = resources.GetString("lblSecurity1.Text");
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new System.Drawing.Point(18, 18);
+            label7.Location = new System.Drawing.Point(21, 24);
             label7.Name = "label7";
-            label7.Size = new System.Drawing.Size(89, 15);
+            label7.Size = new System.Drawing.Size(110, 20);
             label7.TabIndex = 33;
             label7.Text = "Package source";
             // 
             // checkRequireAdmin
             // 
             checkRequireAdmin.AutoSize = true;
-            checkRequireAdmin.Location = new System.Drawing.Point(18, 182);
+            checkRequireAdmin.Location = new System.Drawing.Point(21, 243);
+            checkRequireAdmin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             checkRequireAdmin.Name = "checkRequireAdmin";
-            checkRequireAdmin.Size = new System.Drawing.Size(224, 19);
+            checkRequireAdmin.Size = new System.Drawing.Size(282, 24);
             checkRequireAdmin.TabIndex = 1;
             checkRequireAdmin.Text = "Upgrade with administrator privileges";
             checkRequireAdmin.UseVisualStyleBackColor = true;
@@ -177,9 +194,10 @@
             // checkCacheAdmin
             // 
             checkCacheAdmin.AutoSize = true;
-            checkCacheAdmin.Location = new System.Drawing.Point(18, 232);
+            checkCacheAdmin.Location = new System.Drawing.Point(21, 309);
+            checkCacheAdmin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             checkCacheAdmin.Name = "checkCacheAdmin";
-            checkCacheAdmin.Size = new System.Drawing.Size(276, 19);
+            checkCacheAdmin.Size = new System.Drawing.Size(345, 24);
             checkCacheAdmin.TabIndex = 2;
             checkCacheAdmin.Text = "Cache administrator privileges during upgrades";
             checkCacheAdmin.UseVisualStyleBackColor = true;
@@ -189,9 +207,10 @@
             cmbSource.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             cmbSource.FormattingEnabled = true;
             cmbSource.Items.AddRange(new object[] { "Winget", "Chocolatey" });
-            cmbSource.Location = new System.Drawing.Point(126, 15);
+            cmbSource.Location = new System.Drawing.Point(144, 20);
+            cmbSource.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             cmbSource.Name = "cmbSource";
-            cmbSource.Size = new System.Drawing.Size(288, 23);
+            cmbSource.Size = new System.Drawing.Size(329, 28);
             cmbSource.TabIndex = 0;
             // 
             // lblGSudoStatus
@@ -199,17 +218,18 @@
             lblGSudoStatus.AutoSize = true;
             lblGSudoStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0);
             lblGSudoStatus.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            lblGSudoStatus.Location = new System.Drawing.Point(91, 290);
+            lblGSudoStatus.Location = new System.Drawing.Point(104, 387);
             lblGSudoStatus.Name = "lblGSudoStatus";
-            lblGSudoStatus.Size = new System.Drawing.Size(178, 15);
+            lblGSudoStatus.Size = new System.Drawing.Size(217, 20);
             lblGSudoStatus.TabIndex = 28;
             lblGSudoStatus.Text = "Status (exe not found, or version)";
             // 
             // btnGSudoBinary
             // 
-            btnGSudoBinary.Location = new System.Drawing.Point(339, 264);
+            btnGSudoBinary.Location = new System.Drawing.Point(387, 352);
+            btnGSudoBinary.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             btnGSudoBinary.Name = "btnGSudoBinary";
-            btnGSudoBinary.Size = new System.Drawing.Size(75, 23);
+            btnGSudoBinary.Size = new System.Drawing.Size(86, 31);
             btnGSudoBinary.TabIndex = 8;
             btnGSudoBinary.Text = "Browse";
             btnGSudoBinary.UseVisualStyleBackColor = true;
@@ -217,17 +237,18 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new System.Drawing.Point(18, 267);
+            label6.Location = new System.Drawing.Point(21, 356);
             label6.Name = "label6";
-            label6.Size = new System.Drawing.Size(40, 15);
+            label6.Size = new System.Drawing.Size(50, 20);
             label6.TabIndex = 26;
             label6.Text = "gsudo";
             // 
             // txtGSudoBinary
             // 
-            txtGSudoBinary.Location = new System.Drawing.Point(91, 264);
+            txtGSudoBinary.Location = new System.Drawing.Point(104, 352);
+            txtGSudoBinary.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             txtGSudoBinary.Name = "txtGSudoBinary";
-            txtGSudoBinary.Size = new System.Drawing.Size(242, 23);
+            txtGSudoBinary.Size = new System.Drawing.Size(276, 27);
             txtGSudoBinary.TabIndex = 7;
             // 
             // lblChocoStatus
@@ -235,17 +256,18 @@
             lblChocoStatus.AutoSize = true;
             lblChocoStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0);
             lblChocoStatus.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            lblChocoStatus.Location = new System.Drawing.Point(91, 142);
+            lblChocoStatus.Location = new System.Drawing.Point(104, 189);
             lblChocoStatus.Name = "lblChocoStatus";
-            lblChocoStatus.Size = new System.Drawing.Size(178, 15);
+            lblChocoStatus.Size = new System.Drawing.Size(217, 20);
             lblChocoStatus.TabIndex = 24;
             lblChocoStatus.Text = "Status (exe not found, or version)";
             // 
             // btnChocoBinary
             // 
-            btnChocoBinary.Location = new System.Drawing.Point(339, 116);
+            btnChocoBinary.Location = new System.Drawing.Point(387, 155);
+            btnChocoBinary.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             btnChocoBinary.Name = "btnChocoBinary";
-            btnChocoBinary.Size = new System.Drawing.Size(75, 23);
+            btnChocoBinary.Size = new System.Drawing.Size(86, 31);
             btnChocoBinary.TabIndex = 6;
             btnChocoBinary.Text = "Browse";
             btnChocoBinary.UseVisualStyleBackColor = true;
@@ -253,17 +275,18 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new System.Drawing.Point(18, 119);
+            label4.Location = new System.Drawing.Point(21, 159);
             label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(67, 15);
+            label4.Size = new System.Drawing.Size(83, 20);
             label4.TabIndex = 22;
             label4.Text = "Chocolatey";
             // 
             // txtChocoBinary
             // 
-            txtChocoBinary.Location = new System.Drawing.Point(91, 116);
+            txtChocoBinary.Location = new System.Drawing.Point(104, 155);
+            txtChocoBinary.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             txtChocoBinary.Name = "txtChocoBinary";
-            txtChocoBinary.Size = new System.Drawing.Size(242, 23);
+            txtChocoBinary.Size = new System.Drawing.Size(276, 27);
             txtChocoBinary.TabIndex = 5;
             // 
             // lblWingetStatus
@@ -271,17 +294,18 @@
             lblWingetStatus.AutoSize = true;
             lblWingetStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0);
             lblWingetStatus.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            lblWingetStatus.Location = new System.Drawing.Point(91, 82);
+            lblWingetStatus.Location = new System.Drawing.Point(104, 109);
             lblWingetStatus.Name = "lblWingetStatus";
-            lblWingetStatus.Size = new System.Drawing.Size(178, 15);
+            lblWingetStatus.Size = new System.Drawing.Size(217, 20);
             lblWingetStatus.TabIndex = 20;
             lblWingetStatus.Text = "Status (exe not found, or version)";
             // 
             // btnWingetBinary
             // 
-            btnWingetBinary.Location = new System.Drawing.Point(339, 56);
+            btnWingetBinary.Location = new System.Drawing.Point(387, 75);
+            btnWingetBinary.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             btnWingetBinary.Name = "btnWingetBinary";
-            btnWingetBinary.Size = new System.Drawing.Size(75, 23);
+            btnWingetBinary.Size = new System.Drawing.Size(86, 31);
             btnWingetBinary.TabIndex = 4;
             btnWingetBinary.Text = "Browse";
             btnWingetBinary.UseVisualStyleBackColor = true;
@@ -289,38 +313,30 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(18, 59);
+            label1.Location = new System.Drawing.Point(21, 79);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(45, 15);
+            label1.Size = new System.Drawing.Size(57, 20);
             label1.TabIndex = 18;
             label1.Text = "Winget";
             // 
             // txtWingetBinary
             // 
-            txtWingetBinary.Location = new System.Drawing.Point(91, 56);
+            txtWingetBinary.Location = new System.Drawing.Point(104, 75);
+            txtWingetBinary.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             txtWingetBinary.Name = "txtWingetBinary";
-            txtWingetBinary.Size = new System.Drawing.Size(242, 23);
+            txtWingetBinary.Size = new System.Drawing.Size(276, 27);
             txtWingetBinary.TabIndex = 3;
-            // 
-            // checkEnableGsudo
-            // 
-            checkEnableGsudo.AutoSize = true;
-            checkEnableGsudo.Location = new System.Drawing.Point(18, 207);
-            checkEnableGsudo.Name = "checkEnableGsudo";
-            checkEnableGsudo.Size = new System.Drawing.Size(97, 19);
-            checkEnableGsudo.TabIndex = 37;
-            checkEnableGsudo.Text = "Enable gsudo";
-            checkEnableGsudo.UseVisualStyleBackColor = true;
             // 
             // SettingsWindow
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(648, 435);
+            ClientSize = new System.Drawing.Size(741, 580);
             Controls.Add(panel1);
             Controls.Add(panel2);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             MaximizeBox = false;
             Name = "SettingsWindow";
             Text = "SettingsWindow";
